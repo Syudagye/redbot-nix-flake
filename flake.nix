7ce2e45b120241048e9de9820f73246109fb9852
Syudagye <syudagye@gmail.com>
2022-10-16 03:46:48 +0200

    yay

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Redbot Setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    (flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        instance-name = "muzik_trouville";
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        python = pkgs.python39.withPackages (p: with p; [ pip ]);
+        nativeBuildInputs = with pkgs; [
+          virtualenv
+          jdk11_headless
+        ] ++ [python];
+        shell = pkgs.mkShell {
+          inherit nativeBuildInputs;
+        };
+        run = pkgs.writeShellScript "run-redbot.sh" ''
+          declare -x PATH="${pkgs.jdk11_headless}/bin:$PATH"
+          ${python}/bin/python3.9 -m venv ./redenv
+          source ./redenv/bin/activate
+          python -m pip install -U pip setuptools wheel
+          python -m pip install -U Red-DiscordBot
+          redbot ${instance-name} || redbot-setup --instance-name ${instance-name}
+        '';
+      in
+      {
+        packages.default = run;
+        packages.shell = shell;
+        devShells.default = shell;
+        apps.default = {
+          type = "app";
+          program = "${run.outPath}";
+          inherit nativeBuildInputs;
+        };
+      }));
+}
